cb5d3b6fead98ebd2f60494953ae0c89ba120f7a
Bummo Koo <ksquareatm@gmail.com>
2024-06-29 11:55:03 +0900

    Fix build error

--- HomeP_XR_Mini_Vision/ContentView.swift
+++ HomeP_XR_Mini_Vision/ContentView.swift
@@ -7,14 +7,10 @@
 
 import SwiftUI
 import RealityKit
-import RealityKitContent
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
             Text("Hello, world!")
         }
         .padding()
